4af9aec43f302022b7622baf82109829f75027fa
Abel Cumbane <abelrosac@gmail.com>
2025-02-14 22:11:47 +0100

    Updating dependencies

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Caminho do seu repositório (Altere para o caminho real)
-REPO_PATH="/E:/Auto_Commit/my-app"
+REPO_PATH="/e/Auto_Commit/my-app"
 
 # Mensagem de commit aleatória
 MESSAGES=("Refactoring code" "Bug fix" "Updating dependencies" "Small UI improvements" "Adding logs")
 MESSAGE=${MESSAGES[$RANDOM % ${#MESSAGES[@]}]}
 
 # Gerar log de execução
-echo "$(date) - Iniciando commit automático" >> /E:/Auto_Commit/my-app/auto_commit_log.txt
+echo "$(date) - Iniciando commit automático" >> auto_commit_log.txt
 
 cd "$REPO_PATH" || exit
 
@@ -17,8 +17,8 @@ echo "$(date) - Gerando arquivo de log de commit" > auto_commit_log.txt
 
 # Adiciona, commita e faz push
 git add .
-git commit -m "$MESSAGE" >> /E:/Auto_Commit/my-app/auto_commit_log.txt 2>&1
-git push origin main >> /E:/Auto_Commit/my-app/auto_commit_log.txt 2>&1
+git commit -m "$MESSAGE" >> /e/Auto_Commit/my-app/auto_commit_log.txt 2>&1
+git push origin main >> /e/Auto_Commit/my-app/auto_commit_log.txt 2>&1
 
-echo "$(date) - Commit e push completos" >> /E:/Auto_Commit/my-app/auto_commit_log.txt
+echo "$(date) - Commit e push completos" >> /e/Auto_Commit/my-app/auto_commit_log.txt
 
